4b7c989cc4ffeee4f7f577e36236ce80be0becd3
TungCorn <tung01012005s@gmail.com>
2025-12-17 23:10:21 +0700

    feat: Add initial files for Portfolio-Website and fantastic-winner projects, and update existing HTML, PHP, and configuration files.

---
+++ PHIEU_HOC_TAP/pht_chuong9/code/list.blade.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Website Của Tôi</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+
+<header>
+    <h1>Trang Web CSE485 - Chương 9</h1>
+</header>
+
+<div class="container">
+
+    <form action="{{ route('sinh-vien.store') }}" method="POST">
+        @csrf
+        <div>
+            <label for="ten_sinh_vien">Ten sinh vien: </label>
+            <input type="text"
+                   name="ten_sinh_vien"
+                   value="{{ old('ten_sinh_vien') }}"
+                   required>
+        </div>
+        <div>
+            <label for="email">Email: </label>
+            <input type="text"
+                   name="email"
+                   value="{{ old('email') }}"
+                   required>
+        </div>
+        <div>
+            <button type="submit">Submit</button>
+        </div>
+        <div>
+            <div class="student-list">
+                @foreach($dsSinhvien as $sinhvien)
+                    <tr>
+                        <td>{{ $sinhvien->id }}</td>
+                        <td>{{ $sinhvien->ten_sinh_vien }}</td>
+                        <td>{{ $sinhvien->email }}</td>
+                        <br>
+                    </tr>
+                @endforeach
+            </div>
+        </div>
+    </form>
+
+</div>
+
+<footer>
+    <p>&copy; 2025 - Khoa CNTT - Trường Đại học Thủy Lợi</p>
+</footer>
+
+</body>
+</html>
